8fd8781e817c7f10ff7e9e02372759ef44e1e8d4
Xaiyne <gamesifyyt@gmail.com>
2025-06-02 10:51:38 +1000

    f

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-const auth = require('../middleware/auth');
+const { auth } = require('../middleware/auth');
 const rateLimit = require('express-rate-limit');
 const { body, validationResult } = require('express-validator');
 
